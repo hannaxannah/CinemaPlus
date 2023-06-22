a56a892b6f6f4faa9ec46a28dc231bfe12c91e70
HannahSeol <tjfgkssk9472@gmail.com>
2023-06-22 16:03:08 +0900

    co

--- Project/src/main/java/mypage/controller/MypageMainController.java
+++ Project/src/main/java/mypage/controller/MypageMainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MypageMainController {
 
-	private final String command = "main.my";
+	private final String command = "main.mp";
 	private final String getPage = "mypageMain";
 	
 	@RequestMapping(value = command, method = RequestMethod.GET)
