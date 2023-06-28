d34dd14410d8e63391d58ee03273d5a83b50b9eb
shinjuyoung <bo10820@naver.com>
2023-06-28 12:40:26 +0900

    mypage_commit

--- Project/src/main/java/mypage/controller/MypageWriteReviewController.java
+++ Project/src/main/java/mypage/controller/MypageWriteReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import member.model.MemberBean;
 import movie.model.ReservationBean;
@@ -24,10 +25,17 @@ public class MypageWriteReviewController {
 	ScreenDao screenDao;
 	
 	@RequestMapping(value=command)
-		public String doAction(HttpSession session, Model model) {
+		public String doAction(
+				@RequestParam("reservation_num") String reservation_num,
+				HttpSession session, Model model) {
+			
+			System.out.println(reservation_num);
+		
 			MemberBean mb = (MemberBean) session.getAttribute("loginInfo");
 			String id = mb.getMember_id();
-			List<ReservationBean> List = screenDao.writeReview(id);
+			
+			
+			List<ReservationBean> List = screenDao.writeReview(reservation_num);
 			model.addAttribute("List", List);
 		     
 			return getPage;
